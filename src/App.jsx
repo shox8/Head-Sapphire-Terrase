6922c7e5a497d6ac2aab9f6b2d12557e3c9ca67a
Shoxruh <138918930+Shox2008@users.noreply.github.com>
2024-06-04 18:32:03 +0500

    Font

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <Block>
       <img src={require("./assets/cover.png")} />
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, nihil non. Deserunt dolores reiciendis ex consectetur numquam praesentium quos beatae, illo et, suscipit magni unde quam. Eveniet porro necessitatibus iste!</h1>
     </Block>
   );
 }
