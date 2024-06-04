7731db714eb16449d4857a6cc1a790120dfe29b1
Shoxruh <138918930+Shox2008@users.noreply.github.com>
2024-06-04 12:16:34 +0500

    first commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Block } from "./style";
+
+export default function Home() {
+  return (
+    <Block>
+    </Block>
+  );
+}
---
+++ src/pages/home/style.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Block = styled.div`
+  @media screen and (max-width: 700px) {
+  }
+`;
